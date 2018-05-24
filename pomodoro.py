5f42542fdafdbeb688e603b5dc12aaa388c292c1
dancorrea86 <danielmacielcorrea@gmail.com>
2018-05-23 21:48:59 -0300

    Variaveis para ingles

--- pomodoro.py
+++ pomodoro.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import pygame
 
-class Relogio(object):
+class Pomodoro(object):
 
     def __init__(self, master):
         self.master = master
@@ -11,33 +11,33 @@ class Relogio(object):
         self.master.rowconfigure(0, weight=1)
         self.master.title('Pomodoro')
 
-        self.barraMenu = Menu(master)
+        self.menuBar = Menu(master)
 
         # Definingo opções
-        self.menuOpcoes = Menu(self.barraMenu, tearoff=0)
+        self.optionMenu = Menu(self.menuBar, tearoff=0)
 
-        self.menuConfigTempo = Menu(self.barraMenu, tearoff=0)
-        self.menuOpcoes.add_command(label='Config. time', accelerator='Ctrl+t', compound=LEFT, command=self.configTimeWindow)
+        self.menuConfigTempo = Menu(self.menuBar, tearoff=0)
+        self.optionMenu.add_command(label='Config. time', accelerator='Ctrl+t', compound=LEFT, command=self.configTimeWindow)
 
-        self.opcoes_menusom = Menu(self.barraMenu, tearoff=0)
-        self.menuOpcoes.add_cascade(label='Alarms',  menu=self.opcoes_menusom)
+        self.soundMenuOptions = Menu(self.menuBar, tearoff=0)
+        self.optionMenu.add_cascade(label='Alarms',  menu=self.soundMenuOptions)
 
-        self.opcoesAlarmes = {'1. Firestation Bell': 'sounds/firestation_bell.wav',
+        self.alarmsOptions = {'1. Firestation Bell': 'sounds/firestation_bell.wav',
                            '2. Burglar Bell': 'sounds/burglar_bell.wav'}
 
-        self.alarme = StringVar()
-        self.alarme.set('1. Firestation Bell')
+        self.alarm = StringVar()
+        self.alarm.set('1. Firestation Bell')
 
-        for k in sorted(self.opcoesAlarmes):
-            self.opcoes_menusom.add_radiobutton(label=k, variable=self.alarme, command=self.sound)
+        for k in sorted(self.alarmsOptions):
+            self.soundMenuOptions.add_radiobutton(label=k, variable=self.alarm, command=self.sound)
 
-        self.barraMenu.add_cascade(label='Options', menu=self.menuOpcoes)
+        self.menuBar.add_cascade(label='Options', menu=self.optionMenu)
 
-        self.infoMenu = Menu(self.barraMenu, tearoff=0)
+        self.infoMenu = Menu(self.menuBar, tearoff=0)
         self.infoMenu.add_command(label='Info', accelerator='Ctrl+I', compound=LEFT, command=self.telaInfo)
-        self.barraMenu.add_cascade(label='File', menu=self.infoMenu)
+        self.menuBar.add_cascade(label='File', menu=self.infoMenu)
 
-        self.master.config(menu=self.barraMenu)
+        self.master.config(menu=self.menuBar)
 
         self.frame_show()
 
@@ -62,8 +62,8 @@ class Relogio(object):
         self.labelInfo.grid()
 
     def sound(self):
-        self.som_nome = self.alarme.get()
-        self.som_escolhido = self.opcoesAlarmes.get(self.som_nome)
+        self.som_nome = self.alarm.get()
+        self.som_escolhido = self.alarmsOptions.get(self.som_nome)
 
     def frame_show(self):
         self.frame = Frame(self.master, bg='green')
@@ -97,7 +97,7 @@ class Relogio(object):
         if self.timePomodoro < 0.01:
             self.labelClock['text'] = ('0.00')
             pygame.init()
-            audio = pygame.mixer.Sound(self.opcoesAlarmes.get(self.alarme.get()))
+            audio = pygame.mixer.Sound(self.alarmsOptions.get(self.alarm.get()))
             self.master.lift()
             audio.play()
         elif (self.statusCancel == True):
@@ -110,7 +110,7 @@ class Relogio(object):
 
 def main():
     root = Tk()
-    app = Relogio(root)
+    app = Pomodoro(root)
     root.mainloop()
 
 if __name__ == '__main__':
